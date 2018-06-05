a240862d9cb7110591f9de6ccb1ebcd8647455e4
Oleksandr Pavlovskyi <opavlovskyi@gmail.com>
2018-06-06 00:18:42 +0300

    Added delivery/shipping activity Button styles

--- app/src/main/java/com/stashbank/deliverymanagment/MainActivity.java
+++ app/src/main/java/com/stashbank/deliverymanagment/MainActivity.java
@@ -1,10 +1,17 @@
 package com.stashbank.deliverymanagment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+	private Button deliveryMenuButton;
+	private Button shippingMenuButton;
+	private Button paymentMenuButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,34 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
+
+		deliveryMenuButton = (Button) findViewById(R.id.btn_menu_delivery);
+		shippingMenuButton = (Button) findViewById(R.id.btn_menu_shipping);
+		paymentMenuButton = (Button) findViewById(R.id.btn_menu_payment);
+
+		deliveryMenuButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, DeliveryActivity.class);
+				MainActivity.this.startActivity(intent);
+			}
+		});
+
+		shippingMenuButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, ShippingActivity.class);
+				MainActivity.this.startActivity(intent);
+			}
+		});
+
+		paymentMenuButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// Intent intent = new Intent(MainActivity.this, DeliveryActivity.class);
+				// MainActivity.this.startActivity(intent);
+			}
+		});
+
 	}
 }
